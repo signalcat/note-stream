866e0a35e9d19e02a480fc1ad5daf42799741769
Cathy Zhu <cathyxz@gmail.com>
2016-07-06 21:29:54 +0800

    decomposed course card

--- views/index.jsx
+++ views/index.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var DefaultLayout = require('./layouts/default');
 
+var CourseCard = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3>{this.props.course.title}</h3>
+        <p>{this.props.course.semester}</p>
+        <p>{this.props.course.description}</p>
+      </div>
+    );
+  }
+});
+
 var Index = React.createClass({
   render: function() {
     return (
@@ -8,11 +20,9 @@ var Index = React.createClass({
         <div>
           <h1>Note Stream</h1>
           <h2>Courses</h2>
-          <ul>
             {this.props.courses.map(function(course) {
-               return <li key={course.id}>{course.title}</li>;
+              return <CourseCard course={course}/>;
             })}
-          </ul>
         </div>
       </DefaultLayout>
     );
