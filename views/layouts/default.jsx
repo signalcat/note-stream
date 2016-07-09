7039558976de0bd3f90b72540a6a821e96eecb82
He Zhang <hezhang757@gmail.com>
2016-07-09 01:19:18 -0700

    Add css links

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -7,8 +7,8 @@ var DefaultLayout = React.createClass({
         <head>
           <title>{this.props.title}</title>
           <link href="/css/style.css" rel="stylesheet" type="text/css"></link>
+          <link href='https://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'></link>
         </head>
-        
         <body>{this.props.children}</body>
       </html>
     );
